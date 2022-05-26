4fb9415548a5f0b7fc3d008424860691d02edf3f
juanandres10 <juanandresruizhernandez10@gmail.com>
2022-05-26 16:47:00 +0200

    Contraseña Encriptada
    La contraseña esta encriptada

--- src/Controller/RegistroController.php
+++ src/Controller/RegistroController.php
@@ -2,19 +2,37 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class RegistroController extends AbstractController
 {
     /**
      * @Route("/registro", name="app_registro")
      */
-    public function index(): Response
+    public function index(Request $request, UserPasswordEncoderInterface $encoder): Response
     {
+        $user = new User();
+        $form = $this->createForm( UserType::class, $user);
+	$form->handleRequest($request);
+	if($form->isSubmitted() && $form->isValid()){
+		$em = $this->getDoctrine()->getManager();
+		$user->setBaneado(false);
+		$user->setRoles(['ROLE_USER']);
+		$user->setPassword($encoder->encodePassword($user,$form['password']->getData()));
+		$em->persist($user);
+		$em->flush();
+		$this->addFlash( 'exito', 'Registro exitoso' );
+		return $this->redirectToRoute('app_registro');
+	}
         return $this->render('registro/index.html.twig', [
             'controller_name' => 'RegistroController',
+            'formulario'=>$form->createView()
         ]);
     }
 }
